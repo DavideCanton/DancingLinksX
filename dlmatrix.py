36be20edbc28b805aeaa3fe006dba8eff3961f2e
Davide Canton <davide.canton5@gmail.com>
2019-05-05 00:09:44 +0200

    added license

--- dlmatrix.py
+++ dlmatrix.py
@@ -1,42 +1,12 @@
-__author__ = "Davide Canton"
-
-# /*
-# Copyright (c) 2014, Davide Canton
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-# 1. Redistributions of source code must retain the above copyright
-# notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-# notice, this list of conditions and the following disclaimer in the
-# documentation and/or other materials provided with the distribution.
-# 3. All advertising materials mentioning features or use of this software
-# must display the following acknowledgement:
-# This product includes software developed by the <organization>.
-# 4. Neither the name of the <organization> nor the
-# names of its contributors may be used to endorse or promote products
-# derived from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ''AS IS'' AND ANY
-# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
-# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-# */
-
-#
-# Implementation of Donald Knuth's Dancing Links Sparse Matrix
-# as a circular doubly linked list. (http://arxiv.org/abs/cs/0011047)
-#
+"""
+Implementation of Donald Knuth's Dancing Links Sparse Matrix
+as a circular doubly linked list. (http://arxiv.org/abs/cs/0011047)
+"""
 
 import random
 import numpy as np
+
+__author__ = "Davide Canton"
 
 
 class CannotAddRowsError(Exception):
@@ -292,7 +262,7 @@ def iterate_cell(cell, direction):
         cur = getattr(cur, direction)
 
 
-#TODO to be completed
+# TODO to be completed
 class MatrixDisplayer:
     def __init__(self, matrix):
         dic = {}
@@ -323,8 +293,8 @@ class MatrixDisplayer:
                     else:
                         m[2 * (i + 1), 2 * j] = "X"
 
-        for i in range(-1, self.rows*2):
-            for j in range(0, self.cols*2):
+        for i in range(-1, self.rows * 2):
+            for j in range(0, self.cols * 2):
                 print(m.get((i, j), " "), end="")
             print()
 
@@ -332,7 +302,6 @@ class MatrixDisplayer:
 if __name__ == "__main__":
     def from_dense(row):
         return [i for i, el in enumerate(row) if el]
-
 
     r = [from_dense([1, 0, 0, 1, 0, 0, 1]),
          from_dense([1, 0, 0, 1, 0, 0, 0]),
